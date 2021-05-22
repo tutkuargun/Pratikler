2a4c3f2bee7a974989ad661240ae6c279a060483
Tutku Argun <tutkuargun@outlook.com>
2021-05-23 01:31:38 +0300

    Add files via upload

---
+++ ConsoleProgramlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleProgramlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin :");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba "+name+" "+surname);
+        }
+    }
+}
