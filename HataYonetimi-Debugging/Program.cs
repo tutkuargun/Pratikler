c9dd3069d5c457dea03e798340f119b0dfd52444
Tutku Argun <tutkuargun@outlook.com>
2021-05-26 16:03:42 +0300

    Add files via upload

---
+++ HataYonetimi-Debugging/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HataYonetimi_Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              try
+            {
+                Console.WriteLine("Bir Sayı Giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı:"+ sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("hata"+ ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı.");
+            }
+        }
+    }
+}
